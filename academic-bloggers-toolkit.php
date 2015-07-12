5e308d447333ee63e9625ad916dbb4a8971e83f9
dsifford <dsifford@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-12 18:19:11 +0000

    Minor bug/annoyance fixes (see changelog for full details)
    git-svn-id: https://plugins.svn.wordpress.org/academic-bloggers-toolkit/tags/1.1.3@1197564 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ academic-bloggers-toolkit.php
@@ -0,0 +1,47 @@
+<?php
+
+/*
+ *	Plugin Name: Academic Blogger's Toolkit
+ *	Plugin URI: null
+ *	Description: A Wordpress plugin extending the functionality of Wordpress for Academic Blogging
+ *	Version: 1.1.3
+ *	Author: Derek P Sifford
+ *	Author URI: http://www.twitter.com/flightmed1
+ *	License: GPL3
+ *	License URI: https://www.gnu.org/licenses/gpl-3.0.html
+*/
+
+// Assign Global Variables
+
+$plugin_url = WP_PLUGIN_URL . '/academic-bloggers-toolkit';
+$abt_options = array();
+
+
+// Enqueue Stylesheets
+
+function abt_enqueue_styles() {
+
+	wp_enqueue_style( 'abt_shortcodes_stylesheet', plugins_url('academic-bloggers-toolkit/inc/css/shortcodes.css') );
+
+}
+add_action( 'wp_enqueue_scripts', 'abt_enqueue_styles');
+
+// Tidy Requires
+
+require('inc/shortcodes.php');
+require('inc/tinymce-init.php');
+require('inc/peer-review.php');
+require('inc/options-page.php');
+
+// Uninstall Hook - Clean database of Plugin entries
+
+function abt_uninstall() {
+	delete_option( 'abt_citation_options' );
+	delete_option( 'abt_css_options' );
+	delete_option( 'abt_options' );
+}
+if ( function_exists('register_uninstall_hook') ) {
+	register_uninstall_hook(__FILE__, 'abt_uninstall');
+}
+
+?>
